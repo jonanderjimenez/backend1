5d4a6c24f4b5cee183e3ca3f6b3b6d654e8ebfff
jonanderjimenez <124903732+jonanderjimenez@users.noreply.github.com>
2023-10-26 13:01:13 +0000

    kvboiv

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ let app = express();
 
 app.use(express.static('public'))
 
+app.use(express.urlencoded({extended:false}))
+
 app.get("/", function(req, res) {
 res.send("Kaixo mundua!");
 })
 
+app.get('/bezeroa/:izena', function(req, res) {
+    res.send("Bezeroaren izena: " + req.params.izena);
+ });
+
+ app.get('/bezeroa', function(req, res) {
+    res.send(`Kaixo ${req.query.izena} ${req.query.abizena}`)
+});
 app.post("/bezeroa", function(req,res) {
-    res.send('POST eskaera jaso dut')
+    res.send(`Kaixo ${req.body.izena} ${req.body.abizena}`)
 })
 
 app.listen( 3000, function() {
